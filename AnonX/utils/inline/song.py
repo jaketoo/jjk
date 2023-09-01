ccc08b2f9d20c04427c7a94a2e78b30ccda6f584
jaketoo <140762503+jaketoo@users.noreply.github.com>
2023-09-01 17:58:46 +0300

    Update song.py

--- AnonX/utils/inline/song.py
+++ AnonX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="[𖢿التحديثات𖢿]", url=f"https://t.me/HL_BG",
+                text="[𖢿التحديثات𖢿]", url=f"https://t.me/Mvhmed",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
